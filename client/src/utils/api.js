cc53ab0e1c1b3cc4ace20ae6efa1e6a9c19e2ee3
Anurag Rawat <148265902+AnuragRawat92@users.noreply.github.com>
2026-05-11 13:20:58 +0530

    Update api.js with backend url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,12 @@
 // src/utils/api.js
 import axios from "axios";
 
+//const api = axios.create({
+//  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+//});
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL || "https://ecocreds-backend.onrender.com/api"
 });
 
 // auto attach token
