31e48a1e2e00a8568d3f0bb743d69774efc05b5c
Anurag Rawat <148265902+AnuragRawat92@users.noreply.github.com>
2026-05-11 13:33:24 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -193,7 +193,7 @@ export default function Signup() {
     try {
   //    const res = await axios.post("http://localhost:5000/api/auth/signup", form);
       const res = await axios.post(
-  "https://ecocreds-backend.onrender.com/api/auth/signup",
+  "/auth/signup",
   form
 );
       setToken(res.data.token);
