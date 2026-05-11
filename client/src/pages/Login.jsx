6d80ea6d66ef4a3c2cf6f54e060dd6f228c31bc2
Anurag Rawat <148265902+AnuragRawat92@users.noreply.github.com>
2026-05-11 13:18:12 +0530

    Update Login.jsx with backend url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -134,7 +134,11 @@ export default function Login() {
   const submit = async e => {
     e.preventDefault(); setErr(""); setLoading(true);
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+  //    const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post(
+  "https://ecocreds-backend.onrender.com/api/auth/login",
+  form
+);
       setToken(res.data.token);
       window.location.href = "/dashboard";
     } catch (error) {
