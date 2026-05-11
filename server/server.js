0635726c3c132a5e0873c41c3cd7b577eb953bb3
Anurag Rawat <148265902+AnuragRawat92@users.noreply.github.com>
2026-05-11 13:13:33 +0530

    Update server.js with frontend url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ const ecoRoutes = require('./routes/eco');
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: "https://ecocreds-frontend.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
